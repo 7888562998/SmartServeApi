882a1917fc57d5debb4bad9be2f4cb0f08a6e90d
Dev2 Dev2 <DizicoreDev2@outlook.com>
2026-06-16 16:50:09 +0530

    #add-otp-pages

--- src/services/UserRegister.js
+++ src/services/UserRegister.js
@@ -1,5 +1,6 @@
 const registration = require("../models/signUp");
 const jwt = require("jsonwebtoken");
+const { sendEmail } = require("../utils/sendEmail.js");
 require('dotenv').config();
 
 const signUp = async (req, res) => {
@@ -58,7 +59,109 @@ const logIn = async (req, res) => {
   }
 };
 
+const sendOtp = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    console.log("body", req.body);
+
+    // 1. Find user
+    const user = await registration.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send({ message: "User not found" });
+    }
+
+    // 2. Generate OTP
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    console.log("OTP:", otp);
+
+    // 3. Save OTP in DB (recommended)
+    user.otp = otp;
+    user.otpExpire = Date.now() + 10 * 60 * 1000; // 10 min
+    await user.save();
+
+    // 4. Send email
+    await sendEmail(
+      email,
+      "Your OTP Code",
+      `Your OTP is ${otp}. It is valid for 10 minutes.`
+    );
+
+    // 5. Optional token (NOT required for OTP step)
+    res.status(200).send({
+      message: "OTP sent successfully",
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+
+
+
+const verifyOtp = async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    console.log("verify body:", req.body);
+
+    // 1. Find user
+    const user = await registration.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    // 2. Check OTP exists
+    if (!user.otp) {
+      return res.status(400).send({ message: "OTP not found. Please request again." });
+    }
+
+    // 3. Check expiry
+    if (user.otpExpire < Date.now()) {
+      return res.status(400).send({ message: "OTP expired" });
+    }
+
+    // 4. Check OTP match
+    if (String(user.otp) !== String(otp)) {
+      return res.status(400).send({ message: "Invalid OTP" });
+    }
+
+
+    // 5. Clear OTP after success
+    user.otp = null;
+    user.otpExpire = null;
+    await user.save();
+
+
+
+    jwt.sign(
+      { user: user,role:"staff" },
+      process.env.SECRET_KEY,
+      (error, token) => {
+        console.log("token", token);
+        res.status(200).send({
+          message: "OTP verified successfully",
+          token: token,
+          user: user,
+          role: "staff"
+        });
+      }
+    );
+
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Server error", error });
+  }
+};
+
 module.exports = {
   signUp,
   logIn,
+  sendOtp,
+  verifyOtp
 };
